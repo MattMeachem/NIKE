f466c3ae5187196fcb073aa52e9d85765ec94ec8
Matt Meachem <73957800+MattMeachem@users.noreply.github.com>
2025-10-02 13:03:11 +0100

    none forensics build

--- NIKEV4.ps1
+++ NIKEV4.ps1
@@ -1,6 +1,7 @@
-﻿$Appversion = 4
+$Appversion = 4
 $LogPath = "C:\Background_Files\setup_log.txt"
-$Author = MattMeachem
+$Author - "MattMeachem"
+
 
 # Auto-Elevation
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
@@ -71,11 +72,11 @@ try {
         Log "STACK TRACE: $($_.ScriptStackTrace)"
     }
 
-    # === FORENSIC TOOL DEPLOYMENT SECTION ===
+    # === OPENHASHTAB SECTION ===
 
-    $forensicToolPath = "C:\Background_files\forensictools_1.1_setup.exe"
+    $forensicToolPath = "C:\Background_files\OpenHashTab_Machine_x64.msi"
     if (-not (Test-Path -Path $forensicToolPath)) {
-        Log "[!] Forensic tool not found at path: $forensicToolPath"
+        Log "[!] OPENHASHTAB not found at path: $forensicToolPath"
     } else {
         Log "[*] Disabling all network adapters..."
         Get-NetAdapter | Disable-NetAdapter -Confirm:$false -ErrorAction SilentlyContinue
